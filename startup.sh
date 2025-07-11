8bef8047b216d1dad52859f07291aae47cd9fc2a
John Isley <jisley@bushelpowered.com>
2025-07-11 14:02:59 -0500

    Removed zil

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./rigel -a xelishashv2+zil -o stratum+tcp://us.xel.k1pool.com:9351 -u KrSmzbwo7RQZgcmsZBh1X92L1LCSGjJ8q58 -w ubu1 --zil-countdown --log-file logs/miner.log
+./rigel -a xelishashv2 -o stratum+tcp://us.xel.k1pool.com:9351 -u KrSmzbwo7RQZgcmsZBh1X92L1LCSGjJ8q58 -w ubu1 --log-file logs/miner.log
